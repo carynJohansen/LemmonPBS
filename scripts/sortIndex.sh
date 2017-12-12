462993ad2f3dd6314e43b249ee2b6646b0b3f72e
carynJohansen <cjohansen@ucdavis.edu>
2017-12-12 09:41:10 -0800

    bam sort index fixes

--- scripts/sortIndex.sh
+++ scripts/sortIndex.sh
@@ -6,7 +6,7 @@
 #SBATCH -e /home/caryn89/Projects/LemmonPBS/logs/sortIndex_%j.out
 #SBATCH --time=9:00:00
 #SBATCH --mem=50000
-#SBATCH --array=1-15
+#SBATCH --array=1-16
 
 set -u
 set -e
@@ -18,11 +18,17 @@ module load samtools/1.3.1
 
 bamFile="../combinedBams.filelist"
 
+bambaseFile="../bambase.list"
+
 bam=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $bamFile)
 
+bambase=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $bambaseFile)
 
+sortbam=$bambase.sort.bam
 
 echo $bam
+echo $bambase
+echo $sortbam
 
 pwd
 
@@ -31,8 +37,8 @@ pwd
 
 bam_start=`date +%s`
 
-samtools sort $bam
-samtools index $bam
+samtools sort $bam $sortbam
+samtools index $sortbam
 
 bam_end=`date +%s`
 ((bam_time=$bam_end - $bam_start))
